9c184319307ad553f460bcc84a46d5b4c5d25ec8
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2021-09-12 14:34:42 +0300

    Writing Week_count method for determining the number of full weeks.

---
+++ MyLib/LibraryMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyLib
+{
+    /// Calculation of mathematical functions
+
+    public class LibraryMethods
+    {
+        /// <summary>
+        /// Week_count(number_days)
+        /// </summary>
+        /// <param name="number_days">An integer entered from the keyboard by the user — argument of the Week_count function.</param>
+        /// <returns>Returns the value of the Week_count (number of full weeks)  for a given integer.</returns>
+        public static int Week_count(int number_days)
+        {
+            double week_count = number_days / 7;
+
+            int rounded_week_count = (int)Math.Floor(week_count);
+
+            return rounded_week_count;
+        }
+    }
+}
