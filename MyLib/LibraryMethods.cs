d44b8dd0e421a85eba2bf0aa06a2fb73f2a1fcaa
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2021-09-12 19:48:40 +0300

    Writing a Sum_series method to find the sum of n members of a series.

--- MyLib/LibraryMethods.cs
+++ MyLib/LibraryMethods.cs
@@ -19,5 +19,24 @@ namespace MyLib
 
             return rounded_week_count;
         }
+
+        /// <summary>
+        /// Sum_series(n)
+        /// </summary>
+        /// <param name="n">An integer entered from the keyboard by the user — argument of the Sum_series function.</param>
+        /// <returns>Returns the value of the Sum_series (the sum of a given series)  for a given integer.</returns>
+        public double Sum_row(int n)
+        {
+            double sum = 0;
+
+            for (int i=0; i<n+1; i++)
+            {
+                double a = Math.Pow(-1, n) * (1 / Math.Pow(2, n));
+
+                sum = sum + a;
+            }
+
+            return sum;
+        }
     }
 }
