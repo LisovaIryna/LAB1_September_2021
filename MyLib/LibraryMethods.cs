8e95fc3ad0539a221f6c0d3a3b1e24c747f04ac9
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2021-09-13 10:34:53 +0300

    Writing a Series_sum method to find the sum of a series with a given accuracy. Writing a Function_z method to find the exact value of a function. Writing a Calculation_error method to find a calculation error.

--- MyLib/LibraryMethods.cs
+++ MyLib/LibraryMethods.cs
@@ -25,7 +25,7 @@ namespace MyLib
         /// </summary>
         /// <param name="n">An integer entered from the keyboard by the user — argument of the Sum_series function.</param>
         /// <returns>Returns the value of the Sum_series (the sum of a given series)  for a given integer.</returns>
-        public double Sum_row(int n)
+        public static double Sum_series(int n)
         {
             double sum = 0;
 
@@ -37,6 +37,55 @@ namespace MyLib
             }
 
             return sum;
+        }
+
+        /// <summary>
+        /// Series_sum(eps, n)
+        /// </summary>
+        /// <param name="eps">An double entered from the keyboard by the user — first argument of the Series_sum function.</param>
+        /// <param name="z">An integer entered from the keyboard by the user — second argument of the Series_sum function.</param>
+        /// <returns>Returns the value of the Series_sum (the sum of a given series)  for a given double and integer.</returns>
+        public static double Series_sum(double eps, int z)
+        {
+            double sum = 1;
+            double a = 1;
+            int F = 1;
+            int n = 1;
+
+            while (a > eps)
+            {
+                F = F * n;
+                a = Math.Pow(-1, n) * (1 / (F * z));
+                sum = sum + a;
+                n = n + 1;
+            }
+
+            return sum;
+        }
+
+        /// <summary>
+        /// Function_z(z)
+        /// </summary>
+        /// <param name="z">An integer entered from the keyboard by the user — argument of the Function_z function.</param>
+        /// <returns>Returns the value of the Function_z for a given integer.</returns>
+        public static double Function_z(int z)
+        {
+            double function_z = 1 - z * Math.Log(2);
+
+            return function_z;
+        }
+
+        /// <summary>
+        /// Calculation_error(sum, function_z)
+        /// </summary>
+        /// <param name="sum">An double from the Series_sum function — first argument of the Calculation_error function.</param>
+        /// <param name="function_z">An double from the Function_z function — second argument of the Calculation_error function.</param>
+        /// <returns>Returns the value of the Calculation_error for a given double and double.</returns>
+        public static double Calculation_error(double sum, double function_z)
+        {
+            double calculation_error = (Math.Abs(sum - function_z) / function_z) * 1;
+
+            return calculation_error;
         }
     }
 }
